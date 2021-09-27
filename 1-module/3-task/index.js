8815eb0e459ff4b40789caa6be8b5e5da57b5eae
Tetiana Slyvka <krapa234@gmail.com>
2021-09-27 13:45:11 +0300

    1module-3,4task

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,12 @@
 function ucFirst(str) {
-  // ваш код...
+    if (!str) {
+        return str;
+    } else {
+        return str[0].toUpperCase() + str.slice(1);
+    }
 }
+
+let str = 'вася';
+alert(ucFirst(str));
+str = '';
+alert(ucFirst(str));
--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,6 @@
 function checkSpam(str) {
-  // ваш код...
+    if (str.includes('1XbeT now') || str.includes('free xxxxx')) {
+        return true;
+    } else return false;
+
 }
