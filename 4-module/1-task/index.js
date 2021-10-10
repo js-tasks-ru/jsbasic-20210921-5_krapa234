98d1947590b76554cc934ef5827aef7a143dec7c
Tetiana Slyvka <krapa234@gmail.com>
2021-10-10 13:57:57 +0300

    module4 task3

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,11 @@
 function makeFriendsList(friends) {
-  // ваш код...
+    let list = document.createElement('ul');
+
+    friends.forEach(function(friend) {
+        item = document.createElement('li');
+        list.appendChild(item);
+        item.innerHTML = `${friend.firstName} ${friend.lastName}`;
+
+    });
+    return list;
 }
